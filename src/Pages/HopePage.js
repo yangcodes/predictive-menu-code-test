f153e421ad9ed88059a68dac205f2a90ab3a41c1
Jenny Lin <misslinyang91@gmail.com>
2021-10-16 10:42:47 +1100

    created a basic homepage

---
+++ src/Pages/HopePage.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const HomePage = () => (
+  <>
+    <h1>Welcome to the PredictiveHire Summit 2021</h1>
+    <p>
+      PredictiveHire has won Best Innovation in Algorithmic Bias Mitigation for
+      Fair AI in Recruitment (FAIR™). CogX is the world’s largest celebration of
+      Ai and emerging technology. This is the second time we have been honoured
+      at these awards shortlisted last year for Best Conversational Ai Solution
+      for HR.
+    </p>
+    <p>
+      The big question of this year’s CogX awards “How do we get the next 10
+      years right?” couldn’t be more important to us. We want to give everyone a
+      fair go when it comes to getting a job. It has the potential to have so
+      much impact in addressing bias and structural inequality in the world. We
+      know because we’ve seen it. We’ve helped some of the world’s most trusted
+      consumer brands achieve their DE&I metrics by giving all applicants a fair
+      go.
+    </p>
+    <p>
+      Phai does top-of-funnel interviews for you, saving you time. Everyone gets
+      the same interview, anywhere, anytime and untimed. Also, Phai doesn’t care
+      for what you are, only who you are.
+    </p>
+    <p>
+      Phai is a Recruiter’s best friend Spend time on what really matters. Use
+      Phai as your co-pilot to take care of the grunt work.
+    </p>
+  </>
+);
+
+export default HomePage;
