140fc50eb834699270df685c894a427cea9615ad
Jenny Lin <misslinyang91@gmail.com>
2021-10-16 17:23:24 +1100

    added form input onchange

--- src/Pages/LoginForm.js
+++ src/Pages/LoginForm.js
@@ -7,6 +7,10 @@ function LoginForm({ Submit, error }) {
     password: "",
   });
 
+  const submitHandler = (e) => {
+    e.preventDefault();
+    Submit(details);
+  };
   const cancelHandler = () => {
     window.location.assign("/");
   };
@@ -17,18 +21,38 @@ function LoginForm({ Submit, error }) {
 
         <div className="form-group">
           <label htmlFor="name">Name:</label>
-          <input type="text" name="name" id="name" />
+          <input
+            type="text"
+            name="name"
+            id="name"
+            onChange={(e) => setDetails({ ...details, name: e.target.value })}
+            value={details.name}
+          />
         </div>
         <div className="form-group">
           <label htmlFor="email">Email: </label>
-          <input type="email" name="email" id="email" />
+          <input
+            type="email"
+            name="email"
+            id="email"
+            onChange={(e) => setDetails({ ...details, email: e.target.value })}
+            value={details.email}
+          />
         </div>
         <div className="form-group">
           <label htmlFor="password">Password: </label>
-          <input type="text" name="password" id="password" />
+          <input
+            type="text"
+            name="password"
+            id="password"
+            onChange={(e) =>
+              setDetails({ ...details, password: e.target.value })
+            }
+            value={details.password}
+          />
         </div>
         <div className="form-button">
-          <input type="button" value="Submit" />
+          <input type="button" value="Submit" onClick={submitHandler} />
           <input type="button" value="Cancel" onClick={cancelHandler} />
         </div>
       </div>
