eeff3c617ced8d06bf2576de57d3ee6fec72e5c8
Jenny Lin <misslinyang91@gmail.com>
2021-10-16 16:38:20 +1100

    created login form and login page

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
-const LoginPage = () => (
-  <>
-    <h1>Login Page</h1>
-  </>
-);
+const LoginPage = () => {
+  const adminUser = {
+    name: "Phai",
+    email: "admin@predictive.com",
+    password: "admin123",
+  };
+
+  const [user, setUser] = useState({ name: "", email: "" });
+  const [error, setError] = useState("");
+
+  const Submit = (details) => {
+    console.log(details);
+  };
+
+  const Logout = () => {
+    console.log("You've logged out");
+  };
+  return (
+    <div>
+      {user.email !== "" ? (
+        <div>
+          <h2>
+            Welcome, <span>{user.name}</span>
+            <button onClick={Logout}>Logout</button>
+          </h2>
+        </div>
+      ) : (
+        <LoginForm />
+      )}
+    </div>
+  );
+};
 
 export default LoginPage;
