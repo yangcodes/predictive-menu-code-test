aaaf0a99c29b59b9691fbf2a9317ad711ffbb71b
Jenny Lin <misslinyang91@gmail.com>
2021-10-16 14:04:49 +1100

    linked the blogs witht he blog button

--- src/Pages/BlogListPage.js
+++ src/Pages/BlogListPage.js
@@ -1,8 +1,14 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import blogContent from "./blog-content";
 const BlogListPage = () => (
   <>
     <h1>Blogs</h1>
+    {blogContent.map((blog, key) => (
+      <Link className="blog-list-item" key={key} to={`/blog/${blog.name}`}>
+        <h3>{blog.title}</h3>
+      </Link>
+    ))}
   </>
 );
 
