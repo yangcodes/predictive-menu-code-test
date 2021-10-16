3e2753cc59ba54bfefe21b2dfec3036b3e1c917d
Jenny Lin <misslinyang91@gmail.com>
2021-10-16 11:46:17 +1100

    added the static pages in the router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./App.css";
 import HomePage from "./Pages/HomePage";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import AboutPage from "./Pages/AboutPage";
+import BlogList from "./Pages/BlogList";
+import BlogPage from "./Pages/BlogPage";
+import LoginPage from "./Pages/LoginPage";
+
 function App() {
   return (
     <Router>
       <div className="App">
         <Route path="/" component={HomePage} exact />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/blog-list" component={BlogList} />
+        <Route path="/blog" component={BlogPage} />
+        <Route path="/login" component={LoginPage} />
       </div>
     </Router>
   );
